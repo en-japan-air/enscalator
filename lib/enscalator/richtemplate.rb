6ce157592f4e93f067086aaf1793ee5dd937e43b
David Hagege <david.hagege@gmail.com>
2015-03-05 12:02:59 +0900

    Add code for getting/setting route53 DNS records.

--- lib/enscalator/richtemplate.rb
+++ lib/enscalator/richtemplate.rb
@@ -18,6 +18,36 @@ module Enscalator
       @post_run_block.call
     end
 
+    def get_dns_records(zone_name: nil, region: 'us-east-1')
+      client = Aws::Route53::Client.new(region: region)
+      zone = client.list_hosted_zones[:hosted_zones].select{|x| x.name == zone_name}.first
+      records = client.list_resource_record_sets(hosted_zone_id: zone.id)
+      records.values.flatten.map{|x| {name: x.name, type: x.type, records: x.resource_records.map(&:value)} if x.is_a?(Aws::Structure)}.compact
+    end
+
+    def upsert_dns_record(zone_name: nil, record_name: nil, type: 'A', region: 'us-east-1', values: [])
+      client = Aws::Route53::Client.new(region: region)
+      zone = client.list_hosted_zones[:hosted_zones].select{|x| x.name == zone_name}.first
+
+      client.change_resource_record_sets(
+        hosted_zone_id: zone.id,
+        change_batch: {
+          comment: "dns record for #{record_name}",
+          changes: [
+            {
+              action: "UPSERT",
+              resource_record_set: {
+                name: record_name,
+                type: type,
+                resource_records: values.map{|x| {value: x}},
+                ttl: 300
+              }
+            }
+          ]
+        }
+      )
+    end
+
     def tags_to_properties(tags)
       tags.map { |k,v| {:Key => k, :Value => v}}
     end
