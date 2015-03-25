1eb2adfd5632d98d88747ba98da47ac0b4a1c1e1
Denis Vazhenin <denis.vazhenin@me.com>
2015-03-25 23:37:34 +0900

    Added work in progress template for careercard production rds

---
+++ lib/enscalator/plugins/rds_snapshot.rb
@@ -0,0 +1,71 @@
+module Enscalator
+  module RDS_Snapshot
+    def rds_snapshot_init(snapshot_name,
+                          allocated_storage: 5,
+                          multizone: 'false',
+                          parameter_group: '***REMOVED***',
+                          instance_class: "db.m1.small")
+
+      parameter_allocated_storage "RDS",
+        default: allocated_storage,
+        min: 5,
+        max: 1024
+
+      parameter 'SnapshotId',
+                :Default => snapshot_name,
+                :Description => 'Identifier for the DB snapshot to restore from',
+                :Type => 'String',
+                :MinLength => '1',
+                :MaxLength => '64'
+
+      parameter 'Multizone',
+                :Default => multizone,
+                :Description => 'Multizone deployment',
+                :Type => 'String'
+
+      parameter 'RDSParameterGroup',
+                :Default => parameter_group,
+                :Description => 'Custom parameter group for an RDS database family',
+                :Type => 'String'
+
+      parameter_username "RDS"
+
+      parameter_password "RDS"
+
+      parameter_instance_class "RDS", default: instance_class,
+        allowed_values: %w(db.t1.micro db.m1.small db.m3.medium db.m3.large db.m3.xlarge
+                     db.m3.2xlarge db.r3.large db.r3.xlarge db.r3.2xlarge db.r3.4xlarge
+                     db.r3.8xlarge db.t2.micro db.t2.small db.t2.medium db.m2.xlarge db.m2.2xlarge
+                     db.m2.4xlarge db.cr1.8xlarge db.m1.medium db.m1.large db.m1.xlarge)
+
+      resource "RDSSubnetGroup", :Type => 'AWS::RDS::DBSubnetGroup', :Properties => {
+        :DBSubnetGroupDescription => 'Subnet group within VPC',
+        :SubnetIds => [
+          ref_resource_subnet_a,
+          ref_resource_subnet_c
+        ],
+        :Tags => [{:Key => "Name", :Value => "RDSSubnetGroup"}]
+      }
+
+      resource "RDSInstance", :Type => 'AWS::RDS::DBInstance', :Properties => {
+        :Engine => 'MySQL',
+        :PubliclyAccessible => 'false',
+        :DBSnapshotIdentifier => ref("SnapshotId"),
+        :MultiAZ => ref("Multizone"),
+        :MasterUsername => ref("RDSUsername"),
+        :MasterUserPassword => ref("RDSPassword"),
+        :DBInstanceClass => ref("RDSInstanceClass"),
+        :VPCSecurityGroups => [ ref_resource_security_group ],
+        :DBSubnetGroupName => ref("RDSSubnetGroup"),
+        :DBParameterGroupName => ref("RDSParameterGroup"),
+        :AllocatedStorage => ref("RDSAllocatedStorage"),
+        :Tags => [{:Key => "Name", :Value => "RDSInstance"}]
+      }
+
+      output "RDS EndpointAddress",
+        :Description => "RDS Endpoint Address",
+        :Value => get_att("RDSInstance", 'Endpoint.Address')
+
+    end
+  end
+end
---
+++ lib/enscalator/templates/cc_rds.rb
@@ -0,0 +1,40 @@
+module Enscalator
+  module Templates
+    class CareerCardProductionRDS < Enscalator::EnAppTemplateDSL
+      include RDS_Snapshot
+
+      def tpl
+        description 'Production RDS stack for Career Card'
+
+        pre_run do
+          magic_setup stack_name: 'enjapan-vpc',
+                      region: @options[:region],
+                      start_ip_idx: 24
+        end
+
+        rds_snapshot_init('rds:cc-production-20150306-2015-03-24-15-01',
+                          allocated_storage: 100,
+                          multizone: 'true',
+                          parameter_group: 'careercard-production-mysql',
+                          instance_class: 'db.m3.large')
+
+        post_run do
+          region = @options[:region]
+          stack_name = @options[:stack_name]
+          client = Aws::CloudFormation::Client.new(region: region)
+          cfn = Aws::CloudFormation::Resource.new(client: client)
+
+          stack = wait_stack(cfn, stack_name)
+          host = get_resource(stack, 'AuthEndpointAddress')
+
+          upsert_dns_record(
+              zone_name: 'enjapan.prod.',
+              record_name: "rds.#{stack_name}.enjapan.prod.",
+              type: 'CNAME', region: region, values: [host])
+        end
+      end
+    end
+  end
+end
+
+
