a93e8b43a860b2ec42f3a438e302c2e981e7b6a0
Denis Vazhenin <denis.vazhenin@me.com>
2015-04-19 16:51:31 +0900

    Using dummy credentials for aws sdk specific tests

--- spec/lib/enscalator/helpers_spec.rb
+++ spec/lib/enscalator/helpers_spec.rb
@@ -9,8 +9,10 @@ describe 'Enscalator::Helpers' do
   test_fixture = TestFixture.new
 
   it 'should create valid cloudformation client' do
-    client = test_fixture.cfn_client('us-east-1')
-    expect(client.class).to be Aws::CloudFormation::Client
+    VCR.use_cassette 'aws_sdk_cf_client_init' do
+      client = test_fixture.cfn_client('us-east-1')
+      expect(client.class).to be Aws::CloudFormation::Client
+    end
   end
 
   it 'should raise exception if region is not provided for cloudformation client' do
@@ -19,9 +21,11 @@ describe 'Enscalator::Helpers' do
   end
 
   it 'should create valid cloudformation resource accessor' do
-    client = test_fixture.cfn_client('us-east-1')
-    resource = test_fixture.cfn_resource(client)
-    expect(resource.class).to be Aws::CloudFormation::Resource
+    VCR.use_cassette 'aws_sdk_cf_client_resource' do
+      client = test_fixture.cfn_client('us-east-1')
+      resource = test_fixture.cfn_resource(client)
+      expect(resource.class).to be Aws::CloudFormation::Resource
+    end
   end
 
   it 'should raise exception when resource accessor is used without valid client' do
@@ -30,8 +34,10 @@ describe 'Enscalator::Helpers' do
   end
 
   it 'should create valid client for ec2' do
-    client = test_fixture.ec2_client('us-east-1')
-    expect(client.class).to be Aws::EC2::Client
+    VCR.use_cassette 'aws_sdk_ec2_client_init' do
+      client = test_fixture.ec2_client('us-east-1')
+      expect(client.class).to be Aws::EC2::Client
+    end
   end
 
   it 'should raise exception when region is not provided for ec2 client' do
