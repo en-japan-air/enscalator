b304d05a1fa200faae5ac2f7e381e6a0fe626abf
Bach Phan Trong <bachpt@fsoft.com.vn>
2015-06-16 17:24:30 +0700

    [WIP] Move auto_scale before post_run
    [#95837900]

--- lib/enscalator/templates/career_card_ops.rb
+++ lib/enscalator/templates/career_card_ops.rb
@@ -11,16 +11,15 @@ module Enscalator
 
       def tpl
 
+        @zone_name = 'enjapan.prod.'
+
         # get the latest uploaded image's id
         image = find_ami(ec2_client(region), filters: [{name: 'tag:Name', values: [app_name]}])
                   .images
                   .sort { |a, b| a.creation_date <=> b.creation_date }
                   .last || fail("Cannot find valid image in region: '#{region}'")
 
-        pre_run do
-          pre_setup stack_name: 'enjapan-vpc',
-                    region: @options[:region]
-        end
+        pre_run { pre_setup stack_name: 'enjapan-vpc' }
 
         @db_name = 'CareerCardOps'
 
@@ -30,21 +29,9 @@ module Enscalator
                                  default: 'm3.medium'
 
         rds_init(@db_name)
-        elb_resource_name = elb_init(@options[:stack_name], @options[:region])
+        elb_resource_name = elb_init(@options[:stack_name], @options[:region], zone_name: @zone_name)
 
-        post_run do
-          stack_name = @options[:stack_name]
-          cfn = cfn_resource(cfn_client(@options[:region]))
-
-          stack = wait_stack(cfn, stack_name)
-          host = get_resource(stack, "RDS#{@db_name}EndpointAddress")
-
-          upsert_dns_record(
-              zone_name: 'enjapan.prod.',
-              record_name: "rds.#{stack_name}.enjapan.prod.",
-              type: 'CNAME', values: [host], ttl: 30, region: @options[:region])
-
-          auto_scale_init image.image_id,
+        auto_scale_init image.image_id,
                         auto_scale_name: app_name,
                         launch_config_props: {
                           InstanceType: ref("#{app_name}InstanceClass"),
@@ -59,6 +46,21 @@ module Enscalator
                             ref(elb_resource_name)
                           ]
                         }
+
+        post_run do
+
+          stack_name = @options[:stack_name]
+          cfn = cfn_resource(cfn_client(@options[:region]))
+
+          stack = wait_stack(cfn, stack_name)
+          host = get_resource(stack, "RDS#{@db_name}EndpointAddress")
+
+          upsert_dns_record(
+              zone_name: @zone_name,
+              record_name: "rds.#{stack_name}.enjapan.prod.",
+              type: 'CNAME', values: [host], ttl: 30, region: @options[:region])
+
+          
         end
 
       end
