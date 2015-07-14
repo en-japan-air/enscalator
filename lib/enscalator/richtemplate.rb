a9810c1b4cc78e1311b0c2300a08303bf913638e
Denis Vazhenin <denis.vazhenin@me.com>
2015-07-14 13:47:54 +0900

    Removed redudant/commented-out code

--- lib/enscalator/richtemplate.rb
+++ lib/enscalator/richtemplate.rb
@@ -365,32 +365,6 @@ module Enscalator
       ref("#{role_name}InstanceProfile")
     end
 
-    # TODO: remove
-    # Current generation instance types
-    #
-    # @return [Array] allowed instance types
-    def instance_type
-      %w(t2.micro t2.small t2.medium m4.large m4.xlarge m4.2xlarge m4.4xlarge
-        m4.10xlarge m3.medium m3.large m3.xlarge m3.2xlarge c4.large c4.xlarge
-        c4.2xlarge c4.4xlarge c4.8xlarge c3.large c3.xlarge c3.2xlarge c3.4xlarge
-        c3.8xlarge r3.large r3.xlarge r3.2xlarge r3.4xlarge r3.8xlarge g2.2xlarge
-        g2.8xlarge i2.xlarge i2.xlarge i2.4xlarge i2.8xlarge d2.xlarge d2.2xlarge
-        d2.4xlarge d2.8xlarge)
-    end
-
-    # TODO: remove
-    # @deprecated Will be removed once Amazon fully stops supporting these instances
-    # Previous generation instance types
-    #
-    # @return [Array] allowed instance types
-    def instance_type_obsolete
-      %w(t1.micro m1.small m1.medium m1.large m1.xlarge
-        c1.medium c1.xlarge cc2.8xlarge cg1.4xlarge
-        m2.xlarge m2.2xlarge m2.4xlarge
-        cr1.8xlarge hi1.4xlarge hs1.8xlarge)
-    end
-
-    # TODO: under refactoring
     # Instance type parameter
     #
     # @param [String] instance_name instance name
@@ -401,11 +375,6 @@ module Enscalator
       # check if given type is included in allowed_values and fails if none matched
       allowed = if allowed_values.any? && allowed_values.include?(type)
                   allowed_values
-                  # elsif instance_type.include?(type)
-                  #   instance_type
-                  # elsif instance_type_obsolete.include?(type)
-                  #   warn('Using obsolete instance types')
-                  #   instance_type_obsolete
                 else
                   fail("Instance type \"#{type}\" is not in allowed values: #{allowed_values.join(' ')}")
                 end
