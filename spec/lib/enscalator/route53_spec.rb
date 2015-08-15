c0d8de25a7fbf97a9f3fcee4ba4878bafc7e5e62
Denis Vazhenin <denis.vazhenin@me.com>
2015-08-15 10:35:12 +0900

    Rearranged methods and tests order, reserved methods names

--- spec/lib/enscalator/route53_spec.rb
+++ spec/lib/enscalator/route53_spec.rb
@@ -1,136 +1,5 @@
 require 'spec_helper'
 
-describe 'Enscalator::Route53.create_single_dns_record' do
-
-  let(:app_name) { 'route53_dns_record_test' }
-  let(:description) { 'This is a template for route53 test dns records' }
-  let(:dns_record_template_fixture_default) {
-    route53_test_app_name = app_name
-    route53_test_description = description
-    gen_richtemplate(Enscalator::EnAppTemplateDSL) do
-      @app_name = route53_test_app_name
-      value(Description: route53_test_description)
-      mock_availability_zones
-    end
-  }
-
-  context 'when invoked with default parameters' do
-    it 'should generate resource template for single dns A record entry' do
-      Route53TestDefaultDNSRecord = dns_record_template_fixture_default
-      cmd_opts = default_cmd_opts(Route53TestDefaultDNSRecord.name,
-                                  Route53TestDefaultDNSRecord.name.underscore)
-                   .merge({hosted_zone: 'private.enjapan.test'})
-      route53_template = Route53TestDefaultDNSRecord.new(cmd_opts)
-
-      test_record_name = 'test-entry-default'
-      route53_template.create_single_dns_record(app_name,
-                                                cmd_opts[:stack_name],
-                                                cmd_opts[:hosted_zone],
-                                                test_record_name)
-      dict = route53_template.instance_variable_get(:@dict)
-      expect(dict[:Description]).to eq(description)
-      expect(dict[:Resources]["#{app_name}Hostname"].empty?).to be_falsey
-      test_resources = dict[:Resources]["#{app_name}Hostname"]
-      expect(test_resources[:Type]).to eq('AWS::Route53::RecordSet')
-      properties = test_resources[:Properties]
-      expect(properties[:Name]).to eq(test_record_name)
-      expect(properties[:HostedZoneName]).to eq(cmd_opts[:hosted_zone])
-      expect(properties[:TTL]).to eq(300)
-      expect(properties[:Type]).to eq('A')
-      expect(properties[:ResourceRecords]).to eq(ref("#{app_name}PublicIpAddress"))
-    end
-  end
-
-  context 'when invoked with type parameter set to non-default value' do
-    it 'should include valid dns record type in generated template' do
-      Route53TestDefaultValidRecordType = dns_record_template_fixture_default
-      cmd_opts =
-        default_cmd_opts(Route53TestDefaultValidRecordType.name,
-                         Route53TestDefaultValidRecordType.name.underscore)
-          .merge({hosted_zone: 'private.enjapan.test'})
-      route53_template = Route53TestDefaultValidRecordType.new(cmd_opts)
-
-      test_record_name = 'test-entry-valid-type-default'
-      test_record_type = 'CNAME'
-
-      route53_template.create_single_dns_record(app_name,
-                                                cmd_opts[:stack_name],
-                                                cmd_opts[:hosted_zone],
-                                                test_record_name,
-                                                type: test_record_type)
-      dict = route53_template.instance_variable_get(:@dict)
-      test_resources = dict[:Resources]["#{app_name}Hostname"]
-      expect(test_resources[:Properties][:Type]).to eq(test_record_type)
-    end
-
-    it 'should raise Runtime exception if supplied value is not valid' do
-      Route53TestDefaultNonValidRecordType = dns_record_template_fixture_default
-      cmd_opts =
-        default_cmd_opts(Route53TestDefaultNonValidRecordType.name,
-                         Route53TestDefaultNonValidRecordType.name.underscore)
-          .merge({hosted_zone: 'private.enjapan.test'})
-      route53_template = Route53TestDefaultNonValidRecordType.new(cmd_opts)
-      test_record_name = 'test-entry-wrong-type-default'
-      expect {
-        route53_template.create_single_dns_record(app_name,
-                                                  cmd_opts[:stack_name],
-                                                  cmd_opts[:hosted_zone],
-                                                  test_record_name,
-                                                  type: 'MMX') }.to raise_exception(RuntimeError)
-      expect {
-        route53_template.create_single_dns_record(app_name,
-                                                  cmd_opts[:stack_name],
-                                                  cmd_opts[:hosted_zone],
-                                                  test_record_name,
-                                                  type: '') }.to raise_exception(RuntimeError)
-      expect {
-        route53_template.create_single_dns_record(app_name,
-                                                  cmd_opts[:stack_name],
-                                                  cmd_opts[:hosted_zone],
-                                                  test_record_name,
-                                                  type: {}) }.to raise_exception(RuntimeError)
-    end
-  end
-
-  context 'when invoked with healthcheck parameter with non-default value' do
-    it 'should include valid healthcheck reference in generated template' do
-      Route53TestDefaultValidHealthCheck = dns_record_template_fixture_default
-      cmd_opts =
-        default_cmd_opts(Route53TestDefaultValidHealthCheck.name,
-                         Route53TestDefaultValidHealthCheck.name.underscore)
-          .merge({hosted_zone: 'private.enjapan.test'})
-      route53_template = Route53TestDefaultValidHealthCheck.new(cmd_opts)
-      test_record_name = 'test-entry-healthy-default'
-      test_healthcheck = "#{app_name}Healthcheck"
-      route53_template.create_single_dns_record(app_name,
-                                                cmd_opts[:stack_name],
-                                                cmd_opts[:hosted_zone],
-                                                test_record_name,
-                                                healthcheck: ref(test_healthcheck))
-      dict = route53_template.instance_variable_get(:@dict)
-      test_resources = dict[:Resources]["#{app_name}Hostname"]
-      expect(test_resources[:Properties][:HealthCheckId]).to eq(ref(test_healthcheck))
-    end
-
-    it 'should raise Runtime exception if its not valid' do
-      Route53TestDefaultNonValidHealthCheck = dns_record_template_fixture_default
-      cmd_opts =
-        default_cmd_opts(Route53TestDefaultNonValidHealthCheck.name,
-                         Route53TestDefaultNonValidHealthCheck.name.underscore)
-          .merge({hosted_zone: 'private.enjapan.test'})
-      route53_template = Route53TestDefaultNonValidHealthCheck.new(cmd_opts)
-      test_record_name = 'test-entry-nonhealthy-default'
-      expect {
-        route53_template.create_single_dns_record(app_name,
-                                                  cmd_opts[:stack_name],
-                                                  cmd_opts[:hosted_zone],
-                                                  test_record_name,
-                                                  healthcheck: []) }.to raise_exception(RuntimeError)
-    end
-  end
-end
-
-
 describe 'Enscalator::Route53.create_healthcheck' do
 
   let(:app_name) { 'route53_healthcheck_test' }
@@ -165,8 +34,8 @@ describe 'Enscalator::Route53.create_healthcheck' do
       expect(config[:IPAddress]).to be_nil
       expect(config[:FullyQualifiedDomainName]).to eq(test_fqdn)
       tags = test_resources[:Properties][:HealthCheckTags]
-      expect(tags).to include({Key: 'Application', Value: app_name}) and
-        include({Key: 'Stack', Value: cmd_opts[:stack_name]})
+      expect(tags).to include({ Key: 'Application', Value: app_name }) and
+        include({ Key: 'Stack', Value: cmd_opts[:stack_name] })
     end
   end
 
@@ -202,6 +71,190 @@ describe 'Enscalator::Route53.create_healthcheck' do
                                                    cmd_opts[:stack_name],
                                                    fqdn: test_fqdn,
                                                    type: 'UDP') }.to raise_exception(RuntimeError)
+    end
+  end
+end
+
+describe 'Enscalator::Route53.create_hosted_zone' do
+
+  let(:app_name) { 'route53_hosted_zone_test' }
+  let(:description) { 'This is a template for route53 hosted zone' }
+  let(:hosted_zone_template_fixture_default) {
+    route53_test_app_name = app_name
+    route53_test_description = description
+    gen_richtemplate(Enscalator::EnAppTemplateDSL) do
+      @app_name = route53_test_app_name
+      value(Description: route53_test_description)
+      mock_availability_zones
+    end
+  }
+
+  # TODO: remove before adding tests and method implementation
+  context 'when not implemented method gets invoked' do
+    it 'should raise Runtime exception' do
+      Route53TestNotImplementedHostedZone = hosted_zone_template_fixture_default
+      cmd_opts = default_cmd_opts(Route53TestNotImplementedHostedZone.name,
+                                  Route53TestNotImplementedHostedZone.name.underscore)
+                   .merge({ hosted_zone: 'private.enjapan.test' })
+      route53_template = Route53TestNotImplementedHostedZone.new(cmd_opts)
+      expect { route53_template.create_hosted_zone }.to raise_exception(RuntimeError)
+    end
+  end
+end
+
+describe 'Enscalator::Route53.create_single_dns_record' do
+
+  let(:app_name) { 'route53_dns_record_test' }
+  let(:description) { 'This is a template for route53 test dns records' }
+  let(:dns_record_template_fixture_default) {
+    route53_test_app_name = app_name
+    route53_test_description = description
+    gen_richtemplate(Enscalator::EnAppTemplateDSL) do
+      @app_name = route53_test_app_name
+      value(Description: route53_test_description)
+      mock_availability_zones
+    end
+  }
+
+  context 'when invoked with default parameters' do
+    it 'should generate resource template for single dns A record entry' do
+      Route53TestDefaultDNSRecord = dns_record_template_fixture_default
+      cmd_opts = default_cmd_opts(Route53TestDefaultDNSRecord.name,
+                                  Route53TestDefaultDNSRecord.name.underscore)
+                   .merge({ hosted_zone: 'private.enjapan.test' })
+      route53_template = Route53TestDefaultDNSRecord.new(cmd_opts)
+
+      test_record_name = 'test-entry-default'
+      route53_template.create_single_dns_record(app_name,
+                                                cmd_opts[:stack_name],
+                                                cmd_opts[:hosted_zone],
+                                                test_record_name)
+      dict = route53_template.instance_variable_get(:@dict)
+      expect(dict[:Description]).to eq(description)
+      expect(dict[:Resources]["#{app_name}Hostname"].empty?).to be_falsey
+      test_resources = dict[:Resources]["#{app_name}Hostname"]
+      expect(test_resources[:Type]).to eq('AWS::Route53::RecordSet')
+      properties = test_resources[:Properties]
+      expect(properties[:Name]).to eq(test_record_name)
+      expect(properties[:HostedZoneName]).to eq(cmd_opts[:hosted_zone])
+      expect(properties[:TTL]).to eq(300)
+      expect(properties[:Type]).to eq('A')
+      expect(properties[:ResourceRecords]).to eq(ref("#{app_name}PublicIpAddress"))
+    end
+  end
+
+  context 'when invoked with type parameter set to non-default value' do
+    it 'should include valid dns record type in generated template' do
+      Route53TestDefaultValidRecordType = dns_record_template_fixture_default
+      cmd_opts =
+        default_cmd_opts(Route53TestDefaultValidRecordType.name,
+                         Route53TestDefaultValidRecordType.name.underscore)
+          .merge({ hosted_zone: 'private.enjapan.test' })
+      route53_template = Route53TestDefaultValidRecordType.new(cmd_opts)
+
+      test_record_name = 'test-entry-valid-type-default'
+      test_record_type = 'CNAME'
+
+      route53_template.create_single_dns_record(app_name,
+                                                cmd_opts[:stack_name],
+                                                cmd_opts[:hosted_zone],
+                                                test_record_name,
+                                                type: test_record_type)
+      dict = route53_template.instance_variable_get(:@dict)
+      test_resources = dict[:Resources]["#{app_name}Hostname"]
+      expect(test_resources[:Properties][:Type]).to eq(test_record_type)
+    end
+
+    it 'should raise Runtime exception if supplied value is not valid' do
+      Route53TestDefaultNonValidRecordType = dns_record_template_fixture_default
+      cmd_opts =
+        default_cmd_opts(Route53TestDefaultNonValidRecordType.name,
+                         Route53TestDefaultNonValidRecordType.name.underscore)
+          .merge({ hosted_zone: 'private.enjapan.test' })
+      route53_template = Route53TestDefaultNonValidRecordType.new(cmd_opts)
+      test_record_name = 'test-entry-wrong-type-default'
+      expect {
+        route53_template.create_single_dns_record(app_name,
+                                                  cmd_opts[:stack_name],
+                                                  cmd_opts[:hosted_zone],
+                                                  test_record_name,
+                                                  type: 'MMX') }.to raise_exception(RuntimeError)
+      expect {
+        route53_template.create_single_dns_record(app_name,
+                                                  cmd_opts[:stack_name],
+                                                  cmd_opts[:hosted_zone],
+                                                  test_record_name,
+                                                  type: '') }.to raise_exception(RuntimeError)
+      expect {
+        route53_template.create_single_dns_record(app_name,
+                                                  cmd_opts[:stack_name],
+                                                  cmd_opts[:hosted_zone],
+                                                  test_record_name,
+                                                  type: {}) }.to raise_exception(RuntimeError)
+    end
+  end
+
+  context 'when invoked with healthcheck parameter with non-default value' do
+    it 'should include valid healthcheck reference in generated template' do
+      Route53TestDefaultValidHealthCheck = dns_record_template_fixture_default
+      cmd_opts =
+        default_cmd_opts(Route53TestDefaultValidHealthCheck.name,
+                         Route53TestDefaultValidHealthCheck.name.underscore)
+          .merge({ hosted_zone: 'private.enjapan.test' })
+      route53_template = Route53TestDefaultValidHealthCheck.new(cmd_opts)
+      test_record_name = 'test-entry-healthy-default'
+      test_healthcheck = "#{app_name}Healthcheck"
+      route53_template.create_single_dns_record(app_name,
+                                                cmd_opts[:stack_name],
+                                                cmd_opts[:hosted_zone],
+                                                test_record_name,
+                                                healthcheck: ref(test_healthcheck))
+      dict = route53_template.instance_variable_get(:@dict)
+      test_resources = dict[:Resources]["#{app_name}Hostname"]
+      expect(test_resources[:Properties][:HealthCheckId]).to eq(ref(test_healthcheck))
+    end
+
+    it 'should raise Runtime exception if its not valid' do
+      Route53TestDefaultNonValidHealthCheck = dns_record_template_fixture_default
+      cmd_opts =
+        default_cmd_opts(Route53TestDefaultNonValidHealthCheck.name,
+                         Route53TestDefaultNonValidHealthCheck.name.underscore)
+          .merge({ hosted_zone: 'private.enjapan.test' })
+      route53_template = Route53TestDefaultNonValidHealthCheck.new(cmd_opts)
+      test_record_name = 'test-entry-nonhealthy-default'
+      expect {
+        route53_template.create_single_dns_record(app_name,
+                                                  cmd_opts[:stack_name],
+                                                  cmd_opts[:hosted_zone],
+                                                  test_record_name,
+                                                  healthcheck: []) }.to raise_exception(RuntimeError)
+    end
+  end
+end
+
+describe 'Enscalator::Route53.create_multiple_dns_records' do
+
+  let(:app_name) { 'route53_multi_dns_record_test' }
+  let(:description) { 'This is a template for route53 multiple test dns records' }
+  let(:dns_multi_record_template_fixture_default) {
+    route53_test_app_name = app_name
+    route53_test_description = description
+    gen_richtemplate(Enscalator::EnAppTemplateDSL) do
+      @app_name = route53_test_app_name
+      value(Description: route53_test_description)
+      mock_availability_zones
+    end
+  }
+
+  # TODO: remove before adding tests and method implementation
+  context 'when not implemented method gets invoked' do
+    it 'should raise Runtime exception' do
+      Route53TestNotImplementedDNSRecordGroup = dns_multi_record_template_fixture_default
+      cmd_opts = default_cmd_opts(Route53TestNotImplementedDNSRecordGroup.name,
+                                  Route53TestNotImplementedDNSRecordGroup.name.underscore)
+                   .merge({ hosted_zone: 'private.enjapan.test' })
+      route53_template = Route53TestNotImplementedDNSRecordGroup.new(cmd_opts)
+      expect { route53_template.create_multiple_dns_records }.to raise_exception(RuntimeError)
     end
   end
 end
