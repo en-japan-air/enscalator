2fa9c4f4a666a63767c669a6d389500a76b0241a
Denis Vazhenin <denis.vazhenin@me.com>
2016-01-18 18:49:15 +0900

    Adjusted plugin not to create parameters by default

--- lib/enscalator/plugins/vpc_peering_connection.rb
+++ lib/enscalator/plugins/vpc_peering_connection.rb
@@ -2,17 +2,19 @@ module Enscalator
   module Plugins
     # VPC Peering Connection Plugin
     module VPCPeeringConnection
-
       # Template parameter for VPC ID
       #
       # @param [String] name parameter name
       # @param [String] description parameter description
-      def parameter_vpc_id(name, description)
-        parameter name,
-                  Description: description,
-                  Type: 'String',
-                  AllowedPattern: 'vpc-[a-zA-Z0-9]*',
-                  ConstraintDescription: 'must be valid VPC id (vpc-*).'
+      def parameter_vpc_id(name, description, default_value = nil)
+        options = {
+          Description: description,
+          Type: 'String',
+          AllowedPattern: 'vpc-[a-zA-Z0-9]*',
+          ConstraintDescription: 'must be valid VPC id (vpc-*).'
+        }
+        options[:Default] = default_value if default_value && !default_value.nil?
+        parameter name, options
       end
 
       # Create new vpc peering connection
@@ -20,9 +22,6 @@ module Enscalator
       # @param [String] conn_name connection name
       # @param [Array<String>] tags list of tags
       def vpc_peering_init(conn_name, tags: [])
-        parameter_vpc_id("#{conn_name}VpcId", 'VpcId from where connection gets created')
-        parameter_vpc_id("#{conn_name}PeerVpcId", 'VpcId where peering connection should go')
-
         properties = {
           Type: 'AWS::EC2::VPCPeeringConnection',
           Properties: {
@@ -32,11 +31,7 @@ module Enscalator
         }
 
         # Set plugin tags
-        if properties.key?(:Tags) && !properties[:Tags].empty?
-          properties[:Tags].concat(tags)
-        else
-          properties[:Tags] = tags
-        end
+        properties[:Tags] = tags if tags && !tags.empty?
 
         resource conn_name, properties
       end
--- spec/lib/enscalator/plugins/vpc_peering_connection_spec.rb
+++ spec/lib/enscalator/plugins/vpc_peering_connection_spec.rb
@@ -1,6 +1,44 @@
 require 'spec_helper'
 
 describe Enscalator::Plugins::VPCPeeringConnection do
+
+  describe '#parameter_vpc_id' do
+    let(:test_param_name) { 'somename' }
+    let(:test_param_description) { 'this is somename parameter' }
+    let(:test_param_vpc_id) { 'vpc-12345678' }
+    let(:template_fixture) do
+      gen_richtemplate('sometemplate'.humanize, Enscalator::RichTemplateDSL, [described_class], &proc {})
+    end
+    let(:cmd_opts) { default_cmd_opts(template_fixture.name, template_fixture.name.underscore) }
+
+    context 'when vpc id is given' do
+      it 'uses all fields to generate parameter' do
+        test_template = template_fixture.new(cmd_opts)
+        dict = test_template.instance_variable_get(:@dict)
+        expect(dict).to be_empty
+
+        test_template.parameter_vpc_id(test_param_name, test_param_description, test_param_vpc_id)
+        expect(dict).to include(:Parameters)
+        expect(dict[:Parameters]).to include(test_param_name)
+        test_params = dict[:Parameters][test_param_name]
+        expect(test_params[:Description]).to eq(test_param_description)
+        expect(test_params[:Default]).to eq(test_param_vpc_id)
+      end
+    end
+
+    context 'when vpc id is not given' do
+      it 'uses only name and description to generate parameter' do
+        test_template = template_fixture.new(cmd_opts)
+        dict = test_template.instance_variable_get(:@dict)
+        expect(dict).to be_empty
+
+        test_template.parameter_vpc_id(test_param_name, test_param_description)
+        expect(dict[:Parameters][test_param_name]).not_to include(:Default)
+      end
+    end
+
+  end
+
   describe '#vpc_peering_init' do
     let(:conn_name) { 'test_conn' }
 
@@ -9,7 +47,7 @@ describe Enscalator::Plugins::VPCPeeringConnection do
         vpc_peering_test_conn_name = conn_name
         vpc_peering_test_template_name = conn_name.humanize.delete(' ')
         gen_richtemplate(vpc_peering_test_template_name,
-                         Enscalator::EnAppTemplateDSL,
+                         Enscalator::RichTemplateDSL,
                          [described_class]) do
           mock_availability_zones
           vpc_peering_init(vpc_peering_test_conn_name)
@@ -21,12 +59,10 @@ describe Enscalator::Plugins::VPCPeeringConnection do
         vpc_peering_template = template_fixture.new(cmd_opts)
         dict = vpc_peering_template.instance_variable_get(:@dict)
 
-        parameters_under_test = dict[:Parameters]
-        expect(parameters_under_test.keys).to include(*%W(#{conn_name}VpcId #{conn_name}PeerVpcId))
-
         resource_under_test = dict[:Resources]
         expect(resource_under_test).to include(conn_name)
         expect(resource_under_test[conn_name][:Type]).to eq('AWS::EC2::VPCPeeringConnection')
+        puts 'VPC Peering template resource without tags => ', resource_under_test
         expect { resource_under_test[conn_name].fetch(:Tags) }.to raise_error(KeyError)
         resource_props = resource_under_test[conn_name][:Properties]
         expected_props = {
@@ -51,7 +87,7 @@ describe Enscalator::Plugins::VPCPeeringConnection do
         vpc_peering_test_template_name = conn_name.humanize.delete(' ')
         vpc_peering_test_tags = tags
         gen_richtemplate(vpc_peering_test_template_name,
-                         Enscalator::EnAppTemplateDSL,
+                         Enscalator::RichTemplateDSL,
                          [described_class]) do
           mock_availability_zones
           vpc_peering_init(vpc_peering_test_conn_name, tags: vpc_peering_test_tags)
@@ -63,6 +99,7 @@ describe Enscalator::Plugins::VPCPeeringConnection do
         vpc_peering_template = template_fixture.new(cmd_opts)
         dict = vpc_peering_template.instance_variable_get(:@dict)
         resource_under_test = dict[:Resources]
+        puts 'VPC Peering template resource with tags => ', resource_under_test
         expect(resource_under_test[conn_name][:Tags]).to include(*tags)
       end
     end
