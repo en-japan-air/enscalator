d8ad2d6a62b0086fbfd02771869d59ede7d2da2f
Denis Vazhenin <denis.vazhenin@me.com>
2016-01-28 11:18:43 +0900

    Reorganized tests for elasticache

--- spec/lib/enscalator/plugins/elasticache_spec.rb
+++ spec/lib/enscalator/plugins/elasticache_spec.rb
@@ -1,10 +1,84 @@
 require 'spec_helper'
 
 describe Enscalator::Plugins::ElastiCache do
-  describe '#elasticache_cluster_init' do
-    let(:app_name) { 'el_cluster_test' }
-    let(:description) { 'This is test template for elasticache cluster' }
 
+  let(:app_name) { 'el_cluster_test' }
+  let(:description) { 'This is test template for elasticache cluster' }
+
+  describe '#init_cluster_resources' do
+    let(:cache_node_type) { 'cache.t2.medium' }
+    context 'when invoked with default parameters' do
+      let(:template_fixture) do
+        el_test_app_name = app_name
+        el_test_description = description
+        el_test_template_name = app_name.humanize.delete(' ')
+        el_test_cache_node_type = cache_node_type
+        gen_richtemplate(el_test_template_name,
+                         Enscalator::EnAppTemplateDSL,
+                         [described_class]) do
+          @app_name = el_test_app_name
+          value(Description: el_test_description)
+          mock_availability_zones
+          init_cluster_resources(el_test_app_name, el_test_cache_node_type)
+        end
+      end
+      let(:cmd_opts) { default_cmd_opts(template_fixture.name, template_fixture.name.underscore) }
+
+      it 'generates valid template' do
+        elasticache_common_template = template_fixture.new(cmd_opts)
+        dict = elasticache_common_template.instance_variable_get(:@dict)
+        expect(dict.key?(:Resources)).to be_truthy
+        resources = dict[:Resources]
+
+        # TODO: add more tests for values in each resource group
+
+        # subnet group
+        expect(resources.key?("#{app_name}ElasticacheSubnetGroup")).to be_truthy
+        subnet_group = resources["#{app_name}ElasticacheSubnetGroup"]
+        expect(subnet_group[:Type]).to eq('AWS::ElastiCache::SubnetGroup')
+
+        # security group
+        expect(resources.key?("#{app_name}RedisSecurityGroup")).to be_truthy
+        security_group = resources["#{app_name}RedisSecurityGroup"]
+        expect(security_group[:Type]).to eq('AWS::EC2::SecurityGroup')
+
+        # redis parameter group
+        expect(resources.key?("#{app_name}RedisParameterGroup")).to be_truthy
+        parameter_group = resources["#{app_name}RedisParameterGroup"]
+        expect(parameter_group[:Type]).to eq('AWS::ElastiCache::ParameterGroup')
+      end
+    end
+
+    context 'when invoked with custom parameters' do
+      let(:template_fixture) do
+        el_test_app_name = app_name
+        el_test_description = description
+        el_test_template_name = app_name.humanize.delete(' ')
+        el_test_cache_node_type = cache_node_type
+        gen_richtemplate(el_test_template_name,
+                         Enscalator::EnAppTemplateDSL,
+                         [described_class]) do
+          @app_name = el_test_app_name
+          value(Description: el_test_description)
+          mock_availability_zones
+          init_cluster_resources(el_test_app_name, el_test_cache_node_type)
+        end
+      end
+      let(:cmd_opts) { default_cmd_opts(template_fixture.name, template_fixture.name.underscore) }
+      it 'generates valid template with values corresponding to given parameters' do
+        elasticache_common_template = template_fixture.new(cmd_opts)
+        dict = elasticache_common_template.instance_variable_get(:@dict)
+        expect(dict.key?(:Resources)).to be_truthy
+        resources = dict[:Resources]
+        parameter_group = resources["#{app_name}RedisParameterGroup"]
+        expected_reserved_mem =
+          Enscalator::InstanceType.elasticache_instance_type.max_memory(cache_node_type) / 2
+        expect(parameter_group[:Properties][:Properties][:'reserved-memory']).to eq(expected_reserved_mem)
+      end
+    end
+  end
+
+  describe '#elasticache_cluster_init' do
     context 'when invoked with default parameters' do
       let(:template_fixture) do
         es_test_app_name = app_name
@@ -19,36 +93,14 @@ describe Enscalator::Plugins::ElastiCache do
           elasticache_cluster_init(es_test_app_name)
         end
       end
+      let(:cmd_opts) { default_cmd_opts(template_fixture.name, template_fixture.name.underscore) }
 
       it 'generates valid template' do
-        cmd_opts = default_cmd_opts(template_fixture.name, template_fixture.name.underscore)
         elasticache_cluster_template = template_fixture.new(cmd_opts)
         dict = elasticache_cluster_template.instance_variable_get(:@dict)
         expect(dict[:Description]).to eq(description)
-
-        # all resources
         expect(dict.key?(:Resources)).to be_truthy
         resources = dict[:Resources]
-
-        # TODO: move tests for common resources to its own example group
-
-        # subnet group
-        expect(resources.key?("#{app_name}ElasticacheSubnetGroup")).to be_truthy
-        subnet_group = resources["#{app_name}ElasticacheSubnetGroup"]
-        # TODO: add more tests for generated values
-        expect(subnet_group[:Type]).to eq('AWS::ElastiCache::SubnetGroup')
-
-        # security group
-        expect(resources.key?("#{app_name}RedisSecurityGroup")).to be_truthy
-        security_group = resources["#{app_name}RedisSecurityGroup"]
-        # TODO: add more tests for generated values
-        expect(security_group[:Type]).to eq('AWS::EC2::SecurityGroup')
-
-        # redis parameter group
-        expect(resources.key?("#{app_name}RedisParameterGroup")).to be_truthy
-        parameter_group = resources["#{app_name}RedisParameterGroup"]
-        # TODO: add more tests for generated values
-        expect(parameter_group[:Type]).to eq('AWS::ElastiCache::ParameterGroup')
 
         # redis cluster
         expect(resources.key?("#{app_name}RedisCluster")).to be_truthy
