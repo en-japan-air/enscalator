b13c8bbdb53c82f1567167c537c59e108e42081d
Denis Vazhenin <denis.vazhenin@me.com>
2016-03-24 23:57:25 +0900

    Applied fixes outlined in code review

--- lib/enscalator/richtemplate.rb
+++ lib/enscalator/richtemplate.rb
@@ -450,8 +450,8 @@ module Enscalator
     #
     # @param [String] instance_name name of the instance
     # @param [String] type instance type
-    def parameter_ec2_instance_type(instance_name, type:)
-      type = type ? type : Core::InstanceType.ec2_instance_type.current_generation[:general_purpose].first
+    def parameter_ec2_instance_type(instance_name,
+                                    type: Core::InstanceType.rds_instance_type.current_generation[:general_purpose].first)
       fail("Not supported instance type: #{type}") unless Core::InstanceType.ec2_instance_type.supported?(type)
       warn("Using obsolete instance type: #{type}") if Core::InstanceType.ec2_instance_type.obsolete?(type)
       parameter_instance_type(instance_name,
@@ -463,8 +463,8 @@ module Enscalator
     #
     # @param [String] instance_name name of the instance
     # @param [String] type instance type
-    def parameter_rds_instance_type(instance_name, type:)
-      type = type ? type : Core::InstanceType.rds_instance_type.current_generation[:general_purpose].first
+    def parameter_rds_instance_type(instance_name,
+                                    type: Core::InstanceType.rds_instance_type.current_generation[:general_purpose].first)
       fail("Not supported instance type: #{type}") unless Core::InstanceType.rds_instance_type.supported?(type)
       warn("Using obsolete instance type: #{type}") if Core::InstanceType.rds_instance_type.obsolete?(type)
       parameter_instance_type(instance_name,
