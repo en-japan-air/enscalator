790f7881027eac562c1b61338a1f55cdd82144be
Denis Vazhenin <denis.vazhenin@me.com>
2016-05-12 11:37:02 +0900

    Template for Vibe backend

--- lib/enscalator/templates.rb
+++ lib/enscalator/templates.rb
@@ -21,6 +21,7 @@ require_relative '***REMOVED***'
 require_relative '***REMOVED***'
 require_relative '***REMOVED***'
 require_relative '***REMOVED***'
+require_relative '***REMOVED***'
 
 module Enscalator
   # Namespace for template collection
